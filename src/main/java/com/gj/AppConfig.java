5abf66adcf40911080cc8c45e61335c89a438b83
GongJun <28402674@qq.com>
2020-09-13 10:51:39 +0800

    项目创建

--- src/main/java/com/gj/AppConfig.java
+++ src/main/java/com/gj/AppConfig.java
@@ -1,9 +1,24 @@
 package com.gj;
 
 /**
- * @Description:
- * @Author:GongJun
- * @Date:2020/9/13
+ * @Description: 
+ * @Author: GongJun
+ * @Date: Created in 10:27 2020/9/13
  */
 public class AppConfig {
+    
+    /**
+     * @param: [s]
+     * @description: 
+     * @author: GongJun
+     * @time: Created in 10:47 2020/9/13
+     * @modified: 
+     * @return: java.lang.String
+     **/
+    public String ss(String s){
+        return  s;
+    }
+    
+
+    
 }
