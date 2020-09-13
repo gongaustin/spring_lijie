d1df0591ccd356cc41d6ab1e4cea849bfe81c725
GongJun <28402674@qq.com>
2020-09-13 09:52:37 +0800

    项目创建

---
+++ src/main/java/com/gj/AppConfig.java
@@ -0,0 +1,9 @@
+package com.gj;
+
+/**
+ * @Description:
+ * @Author:GongJun
+ * @Date:2020/9/13
+ */
+public class AppConfig {
+}
---
+++ src/main/java/com/gj/Test.java
@@ -0,0 +1,16 @@
+package com.gj;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Description:
+ * @Author:GongJun
+ * @Date:2020/9/13
+ */
+@Configuration
+@ComponentScan("com.gj")
+@EnableAspectJAutoProxy
+public class Test {
+}
